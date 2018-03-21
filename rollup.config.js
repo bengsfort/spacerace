c6722e83481e0423a254ecd1c37c7f91290cd829
BeefEX <janpolak.lysa@gmail.com>
2018-03-21 14:07:16 +0100

    Project setup

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'js/main.js',
+  output: {
+    file: 'build/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [ babel({
+    exclude: 'node_modules/**'
+  })]
+};
