0cb965885bbaa716ee44ebcc774cc6b7e4d1d290
BeefEX <janpolak.lysa@gmail.com>
2018-03-23 14:28:56 +0100

    Changed the build to use a plugin to generate the html file

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import babel from 'rollup-plugin-babel';
+import htmlTemplate from 'rollup-plugin-generate-html-template';
 
 export default {
   input: 'src/js/main.js',
   output: {
-    file: 'build/bundle.js',
+    file: 'dist/bundle.js',
     format: 'cjs'
   },
-  plugins: [ babel({
-    exclude: 'node_modules/**'
-  })]
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    htmlTemplate({
+      template: 'src/template.html',
+      target: 'index.html'
+    })
+  ]
 };
