3c9a999770db3d9c147ace6b186f6d97ca2183d8
BeefEX <janpolak.lysa@gmail.com>
2018-03-24 08:27:59 +0100

    Added GZip to the production build and reworked the build system

---
+++ targets/dev.js
@@ -0,0 +1,38 @@
+import plugins from "./common";
+import serve from 'rollup-plugin-serve';
+import livereload from "rollup-plugin-livereload";
+
+plugins.push([
+
+    // Starts a server and opens browser window
+    serve({
+        open: true,
+        contentBase: ['dist']
+    }),
+
+    // Starts livereload service
+    livereload()
+]);
+
+export default {
+    // The entry module
+    input: 'src/js/main.js',
+    
+    // Setting the output object
+    output: {
+        file: 'dist/bundle.js',
+        format: 'cjs',
+        sourcemap: true
+    },
+    
+    // Passing the plugins array
+    plugins: plugins,
+    
+    // Watch setup
+    watch: {
+        chokidar : true,
+        include: 'src/**',
+        exclude: 'node_modules/**',
+        clearScreen: false
+    }
+};
